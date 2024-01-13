8f3b5926d75fb97072bbc17bdddd92498503de6e
abnermassimo1 <-v>
2024-01-12 21:19:53 -0600

    correccion

--- 41_MontecarloCUDA.py
+++ 41_MontecarloCUDA.py
@@ -6,61 +6,55 @@
 # ESFM IPN DICIEMBRE 2023
 #====================================
 from __future__ import print_function, absolute_import
-from numba import cuda
+from numba import cuda 
 from numba.cuda.random import create_xoroshiro128p_states
 from numba.cuda.random import xoroshiro128p_uniform_float64
 import numpy as np
-import random
+import random 
 
-#=======================================================
-# Kernel de cuda para simulacion Montecarlo en el GPU
-#=======================================================
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Kernel de CUDA para simulación Montecarlo en el GPU
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 @cuda.jit
 def calcularpi_kernel(rng_states, iteraciones, out):
     ii = cuda.grid(1)
-    #====================================================================
-    # Calcular pi dibujando puntos(x, y) al azar y encontrando
-    # la fraccion de ellos ue cae dentro del circulo unitario
-    #====================================================================
+    #++++++++++++++++++++++++++++++++++++++++++++++
+    # Calcular pi dibujando puntos (x, y) al azar y encontrando 
+    # la fracción de ellos que cae dentro del círculo unitario
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     cae_adentro = 0
     for i in range(iteraciones):
-        # Pares al azar diferentes en (-1,1) para cada proceso ii
+        # Pares al azar diferentes en (-1, -1) para cada proceso ii 
         x = xoroshiro128p_uniform_float64(rng_states, ii)
         y = xoroshiro128p_uniform_float64(rng_states, ii)
-        # Contar los que caen dentro del circulo de radio 1
+        # Contar los que caen dentro del círculo de radio 1
         if x**2 + y**2 <= 1.0:
-            cae_adentro +=1
-
-    #==================================================
-    # Escribir resultado para proceso ii
-    #==================================================
+            cae_adentro += 1
+    #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    # Escribir resultados para el proceso ii
+    #+++++++++++++++++++++++++++++++++++++
     out[ii] = 4.0 * cae_adentro / iteraciones
 
-#=============================
-# Procesos para cuda
-#=============================
+#++++++++++++++++++++++++++++++++++++
+# Procesos para CUDA
+#++++++++++++++++++++++++++++++++++++
 N = 262144
 hilosporbloque = 128
 bloques = int(N/hilosporbloque)
 
-#=================
-# Semillas
-#=================
-seed1 = random.uniform(-1,1)
-print(seed1)
-seed2 = random.seed(seed1)
-print(seed2)
-seed = random.randint(-1000,1000)
-print(seed)
+#+++++++++++++++++++
+# Semilla 
+#+++++++++++++++++++
+seed = random.randint(-1000, 1000)
 rng_states = create_xoroshiro128p_states(hilosporbloque*bloques, seed)
 
-#============================
-# Arreglo de salida(escritura)
-#=============================
+#+++++++++++++++++++++++++++++++++
+# Arreglo de salida (escritura)
+#++++++++++++++++++++++++++++++
 out = np.zeros(hilosporbloque*bloques, dtype=np.float64)
 
-#=========================================
-# Correr en paralelo el kernel de cuda
-#=========================================
+#+++++++++++++++++++++++++++++++++++++++++++
+# Correr en paralelo el kernel de CUDA 
+#+++++++++++++++++++++++++++++++++++++++++
 calcularpi_kernel[bloques, hilosporbloque](rng_states, 100000, out)
-print('pi:', out.mean())
+print('pi:', out.astype(np.float64).mean())
